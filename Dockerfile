5a4cd6fb8f6749845eb3cb8369a588440dbbb7e6
nehmtech <nehmtech.ke@gmail.com>
2026-03-31 20:43:27 +0300

    updated

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN npm install
 
 COPY . .
 
-RUN npm run buil
+RUN npm run build
 
 EXPOSE 4173
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://sample-ecommerce-backend-production.up.railway.app/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
